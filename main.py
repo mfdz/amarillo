724f36da0aaa66cc2fa7bf0d375efdf00e886d85
Frank Gerhardt <fg@gerhardt.io>
2022-04-05 01:59:13 +0200

    Add description etc. to Carpool and StopTime

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ from models.Carpool import Carpool
 from models.StopTime import StopTime
 from services.gtfs import gtfs_rt
 
+
 # https://pydantic-docs.helpmanual.io/usage/settings/
 class Settings(BaseSettings):
     agencies: List[str]
-   
+
+
 settings = Settings(_env_file='prod.env', _env_file_encoding='utf-8')
 
 print("Hello Amarillo!")
 
 app = FastAPI(title="Amarillo - The Carpooling Intermediary",
-              description="""This service allows carpool agencies to publish their trip offers, so routing services may suggest them as trip options. For carpool offers, only the minimum required information (origin/destination, optionally intermediate stops, departure time and a deep link for booking/contacting the driver) needs to be published, booking/contact exchange is to be handled by the publishing agency.""",
+              description="This service allows carpool agencies to publish "
+                          "their trip offers, so routing services may suggest "
+                          "them as trip options. For carpool offers, only the "
+                          "minimum required information (origin/destination, "
+                          "optionally intermediate stops, departure time and a "
+                          "deep link for booking/contacting the driver) needs to "
+                          "be published, booking/contact exchange is to be "
+                          "handled by the publishing agency.",
               version="0.0.1",
               # TODO 404
               terms_of_service="http://mfdz.de/carpool-hub-terms/",
@@ -34,37 +43,40 @@ app = FastAPI(title="Amarillo - The Carpooling Intermediary",
               openapi_tags=[
                   {
                       "name": "carpool",
-                      #"description": "Find out more about Amarillo - the carpooling intermediary",
+                      # "description": "Find out more about Amarillo - the carpooling intermediary",
                       "externalDocs": {
                           "description": "Find out more about Amarillo - the carpooling intermediary",
                           "url": "https://github.com/mfdz/amarillo",
                       },
-                  },
+                  }],
+              servers=[
                   {
-                      "name": "servers",
-                      "url": "https://amarillo.mfdz.de:8000"
+                      "description": "Demo server by MFDZ",
+                      "url": "http://amarillo.mfdz.de:8000"
                   }
               ],
               redoc_url=None
               )
 
-stops0 = [StopTime(id="adsf", name="qwert", lat=45, lon=10), StopTime(id="sss", name="dd", lat=45, lon=10)]
+stops0 = [StopTime(id="de:12073:900340137::6", name="qwert", lat=45, lon=10),
+          StopTime(id="de:12073:900340137::7", name="dd", lat=45, lon=10)]
 
 cp0 = Carpool(
-    id="dieNull", 
-    agency="a", 
-    deeplink="https://ride2go.com/trip/123", 
-    stops = stops0,
-    departureTime="15:00", 
-    departureDate="2022-03-30", 
+    id="dieNull",
+    agency="a",
+    deeplink="https://ride2go.com/trip/123",
+    stops=stops0,
+    departureTime="15:00",
+    departureDate="2022-03-30",
 )
 
 data1 = {
     'id': "Eins",
     'agency': "ride2go",
     'deeplink': "https://ride2go.com/trip/123",
-    'stops': [{'id': "asdf", 'name': "qewrt", 'lat':45, 'lon':9},
-             {'id': "yy", 'name': "zzz", 'lat':45, 'lon':9}],
+    'stops': [
+        {'id': "de:12073:900340137::2", 'name': "abc", 'lat': 45, 'lon': 9},
+        {'id': "de:12073:900340137::3", 'name': "xyz", 'lat': 45, 'lon': 9}],
     'departureTime': "15:00",
     'departureDate': "2022-03-30",
 }
@@ -79,10 +91,10 @@ cp2 = """
   "deeplink": "http://mfdz.de",
   "stops": [
     {
-      "id": "03", "name": "drei", "lat": 45, "lon": 9
+      "id": "de:12073:900340137::4", "name": "drei", "lat": 45, "lon": 9
     },
     {
-      "id": "03b", "name": "drei b", "lat": 45, "lon": 9
+      "id": "de:12073:900340137::5", "name": "drei b", "lat": 45, "lon": 9
     }
   ],
   "departureTime": "12:34",
@@ -91,15 +103,14 @@ cp2 = """
 }
 """
 
-carpools: Dict[str, Carpool] = { 
+carpools: Dict[str, Carpool] = {
     cp0.id: cp0,
     cp1.id: cp1
 }
 
-class Message(BaseModel):
-    message: str
+
 @app.put("/carpool",
-         operation_id = "updatecarpool",
+         operation_id="updatecarpool",
          summary="Update an existing carpool",
          # TODO description="",
          response_model=Carpool,
@@ -115,7 +126,6 @@ class Message(BaseModel):
                     # maybe 405 is not needed?
                     405: {"description": "Validation exception"}})
 async def put_carpool(cp: Carpool):
-
     exists = carpools.get(cp.id) != None
 
     if not exists:
@@ -129,21 +139,19 @@ async def put_carpool(cp: Carpool):
     return cp
 
 
-
 @app.post("/carpool",
           tags=["carpool"])
 async def post_carpool(cp: Carpool) -> Carpool:
-    
     if cp.lastUpdated == None:
-        cp.lastUpdated = datetime.now()   
-       
+        cp.lastUpdated = datetime.now()
+
     exists = carpools.get(cp.id) != None
-    
+
     if exists:
         raise "TODO carpool exist"
-    
+
     carpools[cp.id] = cp
-    
+
     return cp
 
 
@@ -151,26 +159,24 @@ async def post_carpool(cp: Carpool) -> Carpool:
 @app.get("/carpool/{agencyId}/{carpoolId}",
          tags=["carpool"])
 async def get_carpool(agencyId: str, carpoolId: str) -> Carpool:
-    
     exists = carpools.get(carpoolId) != None
-    
+
     if not exists:
         return "TODO carpool does not exist"
-        
+
     return carpools[carpoolId]
-    
-    
+
+
 # TODO make use of agencyId     
 @app.delete("/carpool/{agencyId}/{carpoolId}",
             tags=["carpool"])
 async def delete_carpool(agencyId: str, carpoolId: str):
-
     exists = carpools.get(carpoolId) != None
-    
+
     if not exists:
         return "TODO carpool does not exist"
-    
-    carpools[carpoolId] = None    
+
+    carpools[carpoolId] = None
     return "TODO success "
 
 
@@ -179,12 +185,14 @@ async def read_gtfs_rt(format: str = 'protobuf', tags=["gtfs"]):
     data = gtfs_rt(carpools, format)
     if "json" == format.lower():
         return JSONResponse(content=data)
-    else:  
+    else:
         return Response(content=data, media_type="application/x-protobuf")
+
 
 @app.get("/")
 async def read_root():
-    return { "msg": "Hello Amarillo!" }
+    return {"msg": "Hello Amarillo!"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
