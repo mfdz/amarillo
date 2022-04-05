99e92acbe304bc257600afeb65d09327f0bd02e0
Frank Gerhardt <fg@gerhardt.io>
2022-04-05 14:38:40 +0200

    Add description for GET and DELETE /carpool

--- routers/carpool.py
+++ routers/carpool.py
@@ -16,7 +16,6 @@ router = APIRouter(
 @router.put("/",
             operation_id="updatecarpool",
             summary="Update an existing carpool",
-            # TODO description="",
             response_model=Carpool,
             description="Carpool object that should be updated",
             status_code=status.HTTP_202_ACCEPTED,
@@ -27,15 +26,16 @@ router = APIRouter(
                        # TODO note that automatic validations against the schema
                        # are returned with code 422, also shown in Swagger.
                        # maybe 405 is not needed?
-                       405: {"description": "Validation exception"}})
-async def put_carpool(cp: Carpool = Body(
-    ...,
-    examples=examples,
-)):
+                       405: {"description": "Validation exception"}},
+            )
+async def put_carpool(cp: Carpool = Body(..., examples=examples)
+                      ) -> Carpool:
     exists = carpools.get(cp.id) != None
 
     if not exists:
-        raise HTTPException(status_code=404, detail="Carpool not found")
+        raise HTTPException(
+            status_code=404,
+            detail=f"Carpool with id {cp.id} not found")
 
     if cp.lastUpdated == None:
         cp.lastUpdated = datetime.now()
@@ -45,26 +45,24 @@ async def put_carpool(cp: Carpool = Body(
     return cp
 
 
-@router.post("/")
-async def post_carpool(cp: Carpool
-= Body(
-    ...,
-    operation_id="addcarpool",
-    summary="Add a new carpool",
-    description="Carpool object to be created",
-    responses={
-        status.HTTP_200_OK: {
-            "description": "Carpool created"},
-        # TODO note that automatic validations against the schema
-        # are returned with code 422, also shown in Swagger.
-        # maybe 405 is not needed?
-        status.HTTP_405_METHOD_NOT_ALLOWED: {
-            "description": "Validation exception"},
-        status.HTTP_409_CONFLICT: {
-            "description": "Carpool with this id exists already."}},
-    examples=examples,
-)
-) -> Carpool:
+@router.post("/",
+             operation_id="addcarpool",
+             summary="Add a new carpool",
+             description="Carpool object to be created",
+             responses={
+                 status.HTTP_200_OK: {
+                     "description": "Carpool created"},
+                 # TODO note that automatic validations against the schema
+                 # are returned with code 422, also shown in Swagger.
+                 # maybe 405 is not needed?
+                 status.HTTP_405_METHOD_NOT_ALLOWED: {
+                     "description": "Validation exception"},
+                 status.HTTP_409_CONFLICT: {
+                     "description": "Carpool with this id exists already."}})
+async def post_carpool(cp: Carpool = Body(...,
+                                          examples=examples,
+                                          )
+                       ) -> Carpool:
     if cp.lastUpdated == None:
         cp.lastUpdated = datetime.now()
 
@@ -81,23 +79,57 @@ async def post_carpool(cp: Carpool
 
 
 # TODO make use of agencyId
-@router.get("/{agencyId}/{carpoolId}")
+@router.get("/{agencyId}/{carpoolId}",
+            operation_id="getcarpoolById",
+            summary="Find carpool by ID",
+            response_model=Carpool,
+            description="Find carpool by ID",
+            status_code=status.HTTP_200_OK,
+            # TODO next to the status codes are "Links". There is nothing shown now.
+            # Either show something there, or hide the Links, or do nothing.
+            responses={
+                status.HTTP_404_NOT_FOUND: {"description": "Carpool not found"},
+                # TODO note that automatic validations against the schema
+                # are returned with code 422, also shown in Swagger.
+                # maybe 405 is not needed?
+                # 405: {"description": "Validation exception"}
+            },
+            )
 async def get_carpool(agencyId: str, carpoolId: str) -> Carpool:
     exists = carpools.get(carpoolId) != None
 
     if not exists:
-        return "TODO carpool does not exist"
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Carpool with id {carpoolId} does not exist.")
 
     return carpools[carpoolId]
 
 
 # TODO make use of agencyId
-@router.delete("/{agencyId}/{carpoolId}")
+@router.delete("/{agencyId}/{carpoolId}",
+               operation_id="deletecarpool",
+               summary="Deletes a carpool",
+               description="carpool id to delete",
+               status_code=status.HTTP_200_OK,
+               # TODO next to the status codes are "Links". There is nothing shown now.
+               # Either show something there, or hide the Links, or do nothing.
+               responses={
+                   status.HTTP_404_NOT_FOUND: {
+                       "description": "Carpool not found"},
+                   # TODO note that automatic validations against the schema
+                   # are returned with code 422, also shown in Swagger.
+                   # maybe 405 is not needed?
+                   # 405: {"description": "Validation exception"}
+               },
+               )
 async def delete_carpool(agencyId: str, carpoolId: str):
     exists = carpools.get(carpoolId) != None
 
     if not exists:
-        return "TODO carpool does not exist"
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Carpool with id {carpoolId} does not exist.")
 
     carpools[carpoolId] = None
-    return "TODO success "
+    return "deleted"
