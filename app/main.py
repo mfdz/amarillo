a7ccde21e75ce6673665809acfaedd08d3db7f82
Holger Bruch <hb@mfdz.de>
2022-04-14 10:15:20 +0200

    Load stops and export GTFS in prod env

--- app/main.py
+++ app/main.py
@@ -8,16 +8,15 @@ from pydantic import BaseSettings, Field
 from app.services import stops
 from app.services import trips
 from app.services.carpools import CarpoolService
-
 from app.utils.container import container
-
+import app.services.gtfs_generator as gtfs_generator
 
 # https://pydantic-docs.helpmanual.io/usage/settings/
 from app.views import home
 
-
 class Settings(BaseSettings):
     agencies: List[str] = []
+    env: str = 'DEV'
 
 settings = Settings(_env_file='prod.env', _env_file_encoding='utf-8')
 
@@ -76,29 +75,30 @@ app = FastAPI(title="Amarillo - The Carpooling Intermediary",
 app.include_router(carpool.router)
 app.include_router(gtfs_rt.router)
 
-
 def configure():
     configure_services()
     configure_routing()
     
-
 def configure_routing():
     app.mount('/static', StaticFiles(directory='static'), name='static')
     app.include_router(home.router)
 
 def configure_services():
-    # TODO move to prod settings
     stop_sources = [ 
-    #    { "url": "https://data.mfdz.de/mfdz/stops/custom.csv", "vicinity": 50},
-    #    { "url": "https://data.mfdz.de/mfdz/stops/stops_zhv.csv", "vicinity": 50},
-    #    { "url": "https://data.mfdz.de/mfdz/stops/parkings_osm.csv", "vicinity": 500}
+        { "url": "https://data.mfdz.de/mfdz/stops/custom.csv", "vicinity": 50},
+        { "url": "https://data.mfdz.de/mfdz/stops/stops_zhv.csv", "vicinity": 50},
+        { "url": "https://data.mfdz.de/mfdz/stops/parkings_osm.csv", "vicinity": 500}
     ]
     stop_store = stops.StopsStore()
-    for stops_source in stop_sources:
+    if settings.env == 'PROD':
+      for stops_source in stop_sources:
         stop_store.register_stops(stops_source["url"], stops_source["vicinity"])
     container['stops_store'] = stop_store
     container['trips_store'] = trips.TripStore(stop_store)
     container['carpools'] = CarpoolService(container['trips_store'])
+
+    if settings.env == 'PROD':
+      gtfs_generator.start_schedule()
 
 if __name__ == "__main__":
     configure()
--- app/services/gtfs_generator.py
+++ app/services/gtfs_generator.py
@@ -21,19 +21,13 @@ def generate_gtfs():
 	exporter.export("gtfs/mfdz.gtfs.zip", "target/")
 
 def generate_gtfs_rt():
-	print("Generate GTFS")
-	# TODO access agencies defined via model
-	agencies = [ 
-		GtfsAgency('fg', 'Fahrgemeinschaft.de', 'http://www.fahrgemeinschaft.de', 'Europe/Berlin', 'de', 'hilfe@adac-mitfahrclub.de'),
-		GtfsAgency('mifaz', 'mifaz.de', 'http://www.mifaz.de', 'Europe/Berlin', 'de', 'info@mifaz.de'),
-	]
-	feed_info = GtfsFeedInfo('mfdz', 'MITFAHR|DE|ZENTRALE', 'http://www.mitfahrdezentrale.de', 'de', 1)
-	exporter = GtfsExport(agencies, feed_info, trip_store, stop_store)
-	# TODO generate region specific feed
-	exporter.export("gtfs/mfdz.gtfs.zip", "target/")
+	print("Generate GTFS-RT")
+	rt = GtfsRtProducer().generate_feed(time.time())
+	# TODO write out pbf
+	# TODO re-link to minimize
 
 def start_schedule():
-	schedule.every().day.at("00:00").do(generate_gtfs)
-	schedule.every(30).seconds.do(generate_gtfs_rt)
+	schedule.every().day.at("10:30").do(generate_gtfs)
+	# schedule.every(30).seconds.do(generate_gtfs_rt)
 	job_thread = threading.Thread(target=run_schedule, daemon=True)
 	job_thread.start()
