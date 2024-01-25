862f82547ce765a5ecd1c6aa1eca24c3297795f2
Francia Csaba <cf@gerhardt.io>
2024-01-25 15:33:03 +0100

    metrics secrets

--- amarillo/services/secrets.py
+++ amarillo/services/secrets.py
@@ -1,10 +1,13 @@
 from typing import Dict
 from pydantic import Field
 from pydantic_settings import BaseSettings
-
+from typing import Optional
 # Example: secrets = { "mfdz": "some secret" }
 class Secrets(BaseSettings):
     ride2go_token: str = Field(None, env = 'RIDE2GO_TOKEN')
+    # TODO: define these as required if metrics plugin is installed
+    metrics_user: Optional[str] = Field(None, env = 'METRICS_USER')
+    metrics_password: Optional[str] = Field(None, env = 'METRICS_PASSWORD')
 
 
 # Read if file exists, otherwise no error (it's in .gitignore)
