a174c404044477bfa10f69ee7623d67206aa1653
Francia Csaba <cf@gerhardt.io>
2024-01-30 15:09:00 +0100

    moved 'configure_enhancer_services' to enhancer plugin

--- amarillo/configuration.py
+++ amarillo/configuration.py
@@ -1,21 +1,14 @@
 # separate file so that it can be imported without initializing FastAPI
 from amarillo.utils.container import container
-import json
 import logging
-from glob import glob
 
-from amarillo.models.Carpool import Agency, Carpool, Region
-from amarillo.services import stops
-from amarillo.services import trips
 from amarillo.services.agencyconf import AgencyConfService, agency_conf_directory
-from amarillo.services.carpools import CarpoolService
 from amarillo.services.agencies import AgencyService
 from amarillo.services.regions import RegionService
 
 from amarillo.services.config import config
 
 from amarillo.utils.utils import assert_folder_exists
-import amarillo.services.gtfs_generator as gtfs_generator
 
 logger = logging.getLogger(__name__)
 
@@ -48,42 +41,6 @@ def configure_services():
     logger.info("Loaded %d regions", len(container['regions'].regions))
 
     create_required_directories()
-
-
-def configure_enhancer_services():
-    configure_services()
-
-    logger.info("Load stops...")
-    with open(config.stop_sources_file) as stop_sources_file:
-        stop_sources = json.load(stop_sources_file)
-        stop_store = stops.StopsStore(stop_sources)
-    
-    stop_store.load_stop_sources()
-    container['stops_store'] = stop_store
-    container['trips_store'] = trips.TripStore(stop_store)
-    container['carpools'] = CarpoolService(container['trips_store'])
-
-    logger.info("Restore carpools...")
-
-    for agency_id in container['agencies'].agencies:
-        for carpool_file_name in glob(f'data/carpool/{agency_id}/*.json'):
-            try:
-                with open(carpool_file_name) as carpool_file:
-                    carpool = Carpool(**(json.load(carpool_file)))
-                    container['carpools'].put(carpool.agency, carpool.id, carpool)
-            except Exception as e:
-                logger.warning("Issue during restore of carpool %s: %s", carpool_file_name, repr(e))
-                    
-        # notify carpool about carpools in trash, as delete notifications must be sent
-        for carpool_file_name in glob(f'data/trash/{agency_id}/*.json'):
-            with open(carpool_file_name) as carpool_file:
-                carpool = Carpool(**(json.load(carpool_file)))
-                container['carpools'].delete(carpool.agency, carpool.id)
-
-    logger.info("Restored carpools: %s", container['carpools'].get_all_ids())
-    logger.info("Starting scheduler")
-    gtfs_generator.start_schedule()
-
 
 def configure_admin_token():
     if config.admin_token is None:
