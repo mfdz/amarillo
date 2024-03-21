036fd0c35e337dc2df1b9a54204dcf38114e120b
Holger Bruch <hb@mfdz.de>
2024-03-21 20:08:53 +0100

    allow configuration of AMARILLO_BASEURL

--- amarillo/services/config.py
+++ amarillo/services/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
+    amarillo_baseurl: str = 'http://localhost:8000/'
     admin_token: str
     ride2go_query_data: str
     env: str = 'DEV'
