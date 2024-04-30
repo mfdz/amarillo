da06bdefb81e0e70c94e40e65837a06f7fc2bf27
Holger Bruch <hb@mfdz.de>
2024-04-30 17:55:26 +0200

    feat: add config parameter max_age_carpool_offers_in_days (default=180)

--- amarillo/services/config.py
+++ amarillo/services/config.py
@@ -9,5 +9,6 @@ class Config(BaseSettings):
     env: str = 'DEV'
     graphhopper_base_url: str = 'https://api.mfdz.de/gh'
     stop_sources_file: str = 'conf/stop_sources.json'
+    max_age_carpool_offers_in_days: int = 180
 
 config = Config(_env_file='config', _env_file_encoding='utf-8')
