8ca0856d478fee193a8bb1716fa9f517e439f2d2
Holger Bruch <hb@mfdz.de>
2025-01-06 09:56:12 +0100

    fix pydantic deprecation

--- amarillo/services/secrets.py
+++ amarillo/services/secrets.py
@@ -4,7 +4,8 @@ from pydantic_settings import BaseSettings
 
 # Example: secrets = { "mfdz": "some secret" }
 class Secrets(BaseSettings):
-    ride2go_token: str = Field(None, env = 'RIDE2GO_TOKEN')
+    ride2go_token: str = Field(None)
+    # TODO: admin_token should be moved here instead of config
 
 
 # Read if file exists, otherwise no error (it's in .gitignore)
