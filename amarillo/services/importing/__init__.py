b9a8168ace1ca29bd6d67e1c5217df32ed42d833
Holger Bruch <hb@mfdz.de>
2025-03-04 17:12:43 +0100

    feat: add mycarpoolapp importer

--- amarillo/services/importing/__init__.py
+++ amarillo/services/importing/__init__.py
@@ -1,5 +1,6 @@
 from .amarillo import AmarilloImporter
 from .bessermitfahren import BessermitfahrenImporter
+from .mycarpoolapp import MyCarpoolAppImporter
 from .noi import NoiImporter
 from .pendlerportal import PendlerportalImporter
 from .ride2go import Ride2GoImporter
--- amarillo/services/secrets.py
+++ amarillo/services/secrets.py
@@ -7,6 +7,7 @@ class Secrets(BaseSettings):
     ride2go_token: str = Field(None)
     bessermitfahren_url: Optional[str] = Field(None)
     pendlerportal_url: Optional[str] = Field(None)
+    mycarpoolapp_url: Optional[str] = Field(None)
 
     # TODO: admin_token should be moved here instead of config
 
