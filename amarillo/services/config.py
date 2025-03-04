694b8db69fd6bce6adb3135351e2c9e527c50b4b
Holger Bruch <hb@mfdz.de>
2025-03-04 16:55:02 +0100

    feat: run nightly sync for pull agencies

--- amarillo/services/config.py
+++ amarillo/services/config.py
@@ -10,5 +10,8 @@ class Config(BaseSettings):
     graphhopper_base_url: str = 'https://api.mfdz.de/gh'
     stop_sources_file: str = 'conf/stop_sources.json'
     max_age_carpool_offers_in_days: int = 180
+    # Syn per default at 11:30pm so all updates are done at midnight
+    # when gtfs re-generation will happen
+    daily_sync_time: str = '23:00'
 
 config = Config(_env_file='config', _env_file_encoding='utf-8')
