1e7127c54fac2b53b3012f702d8c829722ccd0dd
Holger Bruch <hb@mfdz.de>
2025-03-20 18:15:39 +0100

    fix: allow admin_token to be unset for enhancer

--- amarillo/services/config.py
+++ amarillo/services/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     amarillo_baseurl: str = 'http://localhost:8000/'
-    admin_token: str
+    admin_token: str | None = None
     ride2go_query_data: str
     env: str = 'DEV'
     graphhopper_base_url: str = 'https://api.mfdz.de/gh'
